aca61b08dc4dad61c7c6d65b310973e982e591e3
Ro11ingButler <rollingkai1214@gmail.com>
2023-07-21 16:18:59 +0800

    Add ACE in tutorial

--- src/components/Help.tsx
+++ src/components/Help.tsx
@@ -30,14 +30,14 @@ export default function Help() {
     return (
         <>
             <div className="absolute top-0 h-full w-full flex justify-center items-center transition-opacity duration-300 backdrop-blur-3xl " ref={bannerRef}>
-                <div className="w-5/6 h-5/6 tall:h-4/6 taller:h-3/5 flex flex-col items-center justify-center border-y border-y-white/50 backdrop-blur-3xl bg-[#12192c]/95 gap-2">
+                <div className="w-full h-full tall:w-5/6 tall:h-5/6 taller:h-4/6 flex flex-col items-center justify-center border-y border-y-white/50 backdrop-blur-3xl bg-[#12192c]/95 gap-6 tall:gap-5">
                     <p className="text-center  text-3xl font-[PT-Serif] tracking-widest border-b border-b-white/40 pb-2 text-white/70">
                         Don't exceed 21!
                     </p>
                     <p className="text-2xl ">
                         How to play
                     </p>
-                    <ul className="px-8 flex flex-col gap-2 list-disc">
+                    <ul className="px-8 flex flex-col gap-4 list-disc">
                         <li>
                             Defeat the dealer by having a hand value of exactly 21 or having a higher hand value than the dealer.
                         </li>
@@ -45,7 +45,10 @@ export default function Help() {
                             If a player exceeds the hand value of 21, their hand is considered BUSTED. A BUSTED hand always loses.
                         </li>
                         <li>
-                            If all players are BUSTED, the one who has a hand value closest to 21 wins.
+                            If all players are BUSTED, the one who has a hand value closest to 21 wins. 
+                        </li>
+                        <li>
+                            ACE's value is 11. If you BUST with an ACE, the ACE's value becomes 1 instead.
                         </li>
                     </ul>
                     <Button onClick={handleGotItClick}>GOT IT</Button>
